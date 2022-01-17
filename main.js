9c2cf884718f054fe109e7d6e0fd6cdc9efa04bb
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2022-01-17 18:15:32 +0100

    correzione secondo modo

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-//Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
-
+//Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e eta.
 const studente = {
     nome : 'Gianni',
     cognome : 'Rodari',
-    età : 23
+    eta : 23
 }
-
 //Stampare in console attraverso un ciclo for-in tutte le proprietà dell’oggetto.
+
+console.log("Stampare in console attraverso un ciclo for tutte le proprietà dell’oggetto.");
+
 for(key in studente){
     console.log(key);
     console.log(studente[key]);
 };
+console.log("***************");
 
 //Creare un array di oggetti di studenti.
+console.log("Creare un array di oggetti di studenti.");
+
 const studente1 = {
     nome : 'Mattia',
     cognome : 'Cosenti',
-    età : 24
+    eta : 24
 };
 const studente2 = {
     nome : 'Giorgio',
     cognome : 'Cavalli',
-    età : 22
+    eta : 22
 };
 const studente3 = {
     nome : 'Giulia',
     cognome : 'Fornari',
-    età : 21
+    eta : 21
 };
 
 let listaStudenti = [studente1, studente2, studente3];
@@ -37,14 +41,16 @@ for(let i = 0; i < listaStudenti.length; i++){
     console.log(selected['nome'] + ' ' + selected['cognome']);
     
 };
+console.log("***************");
 
-//Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. con le funzioni
+//Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e eta. con le funzioni
 
+console.log("Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e eta. con le funzioni");
 
-function Studente(nome, cognome, età ){
+function Studente(nome, cognome, eta ){
     this.nome = nome;
     this.cognome = cognome;
-    this.età = età;
+    this.eta = eta;
 }
 
 function RegistraStudente(){
@@ -53,10 +59,10 @@ function RegistraStudente(){
     for(numero; numero > 0;numero--){
         let nome = prompt("Nome dello studente?");
         let cognome = prompt("Cognome dello studente?");
-        let età = prompt("Età dello studente?");
+        let eta = prompt("eta dello studente?");
 
 
-        nuovaListaStudenti[numero] = new Studente(nome, cognome, età);
+        nuovaListaStudenti[numero] = new Studente(nome, cognome, eta);
     }
     return nuovaListaStudenti;
 }
@@ -69,19 +75,24 @@ for(let i = 0;i < listaFInale.length;i++){
         console.log(selezionato[key]);
     }
 }
+console.log("***************");
 
 //seconda versione del compito.
+
+console.log("seconda versione del compito.");
 
 let studenteDiverso = {};
 
 studenteDiverso.nome = prompt("Nome dello studente");
 studenteDiverso.cognome = prompt("Cognome dello studente");
-studenteDiverso.eta = prompt("Età dello studente");
+studenteDiverso.eta = prompt("eta dello studente");
 
 listaStudenti.push(studenteDiverso);
 
 for(let j = 0; j < listaStudenti.length;j++){
-    for(pippo in listaStudenti){
-        console.log(listaStudenti[pippo]);
+    let geppetto = listaStudenti[j];
+    for(pippo in geppetto){
+        console.log(geppetto[pippo]);
     }
 };
+console.log("***************");
